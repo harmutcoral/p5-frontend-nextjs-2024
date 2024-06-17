5f1d389e117b2c6a7f71deb51d46b2942908b683
harmutcoral <coral.harmut98@gmail.com>
2024-06-17 14:31:49 +0200

    add some components and routing

---
+++ app/details/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+export default function Page() {
+  return (
+    <main>
+      <h1>Hey</h1>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
 import { Button } from "../components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
+import Link from "next/link";
 
-export default async function HomePage() {
+export default function HomePage() {
   return (
-    <div>
+    <main className="p-4">
       <h1>Catálogo de Personajes</h1>
-      <Button>Test</Button>
-    </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Test</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-row gap-2">
+          <Link
+            href="/details"
+            className="text-blue-500 hover:underline cursor-pointer"
+          >
+            Go to details
+          </Link>
+          <Button className="flex p-4">Test</Button>
+          <Button variant="secondary">Another test</Button>
+        </CardContent>
+      </Card>
+    </main>
   );
 }
